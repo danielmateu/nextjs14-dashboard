a3e5e21fbde56ff8eae58e000e522199ad1dd614
Daniel Mateu <danielmateu86@gmail.com>
2023-11-23 16:27:47 +0100

    creacion rutas

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,4 @@
-import { montserrat } from './ui/fonts';
+import { lusitana, montserrat } from './ui/fonts';
 import './ui/global.css';
 
 export default function RootLayout({
@@ -10,6 +10,9 @@ export default function RootLayout({
     <html lang="en">
       <body className={`${montserrat.className} antialiased`}>
         {children}
+        <footer className='text-center'>
+          Made with ❤️ by <a href="https://daniel-mateu.vercel.app/" target='_blank'>Daniel Mateu</a>
+        </footer>
       </body>
     </html >
   );
--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,8 @@ import { ArrowRightIcon } from '@heroicons/react/24/outline';
 import Link from 'next/link';
 
 import styles from './ui/home.module.css'
+import { lusitana } from './ui/fonts';
+import Image from 'next/image';
 
 export default function Page() {
   return (
@@ -24,11 +26,26 @@ export default function Page() {
             href="/login"
             className="flex items-center gap-5 self-start rounded-lg bg-blue-500 px-6 py-3 text-sm font-medium text-white transition-colors hover:bg-blue-400 md:text-base"
           >
-            <span>Log in</span> <ArrowRightIcon className="w-5 md:w-6" />
+            <span className={`${lusitana.className}`}>Log in</span> <ArrowRightIcon className="w-5 md:w-6" />
           </Link>
         </div>
         <div className="flex items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12">
           {/* Add Hero Images Here */}
+          {/* <img src="/hero-desktop.png" alt="Hero image for desktop"/> */}
+          <Image
+            className='hidden sm:block'
+            src={'/hero-desktop.png'}
+            width={1000}
+            height={760}
+            alt='Hero Image for Desktop'
+          />
+          <Image
+            className='sm:hidden'
+            src={'/hero-mobile.png'}
+            width={1000}
+            height={760}
+            alt='Hero Image for Desktop'
+          />
         </div>
       </div>
     </main>
