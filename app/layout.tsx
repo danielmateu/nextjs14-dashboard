02c3644a6a408de57a3c9f8a534a1270ab1a4b45
Daniel Mateu <danielmateu86@gmail.com>
2023-11-23 15:39:08 +0100

    añadiendo fuentes

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+import { montserrat } from './ui/fonts';
 import './ui/global.css';
 
 export default function RootLayout({
@@ -7,8 +8,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>
-        {/* <h1>Dashboard</h1> */}
+      <body className={`${montserrat.className} antialiased`}>
         {children}
       </body>
     </html >
