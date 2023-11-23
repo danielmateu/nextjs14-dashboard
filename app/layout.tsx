a84fb78dbfbba739f9572e17fb6e91dbde181e13
Daniel Mateu <danielmateu86@gmail.com>
2023-11-23 16:44:02 +0100

    adding dashboard layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,6 +8,12 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <meta charSet="utf-8" />
+        <title>Nextjs 14 first contact</title>
+        <meta name="description" content="Next.js + TypeScript" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </head>
       <body className={`${montserrat.className} antialiased`}>
         {children}
         <footer className='text-center'>
--- app/page.tsx
+++ app/page.tsx
@@ -11,7 +11,7 @@ export default function Page() {
     <main className="flex min-h-screen flex-col p-6">
       <div className="flex h-20 shrink-0 items-end rounded-lg bg-blue-500 p-4 md:h-52">
         {/* <AcmeLogo /> */}
-        <div className={styles.shape}></div>
+        {/* <div className={styles.shape}></div> */}
       </div>
       <div className="mt-4 flex grow flex-col gap-4 md:flex-row">
         <div className="flex flex-col justify-center gap-6 rounded-lg bg-gray-50 px-6 py-10 md:w-2/5 md:px-20">
@@ -32,6 +32,9 @@ export default function Page() {
         <div className="flex items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12">
           {/* Add Hero Images Here */}
           {/* <img src="/hero-desktop.png" alt="Hero image for desktop"/> */}
+          {/* <div
+            className="h-0 w-0 border-b-[30px] border-l-[20px] border-r-[20px] border-b-black border-l-transparent border-r-transparent"
+          /> */}
           <Image
             className='hidden sm:block'
             src={'/hero-desktop.png'}
