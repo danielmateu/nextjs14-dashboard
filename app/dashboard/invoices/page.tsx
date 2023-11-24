267f4f539181487dd9b3fcbadf315a79ca5dbcc6
Daniel Mateu <danielmateu86@gmail.com>
2023-11-24 12:10:12 +0100

    mostrando la tambla de usuarios

--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -1,9 +1,14 @@
-import { lusitana } from "@/app/ui/fonts"
-import { CreateInvoice } from "@/app/ui/invoices/buttons"
-import Search from "@/app/ui/search"
+import Pagination from '@/app/ui/invoices/pagination';
+import Search from '@/app/ui/search';
+import Table from '@/app/ui/invoices/table';
+import { CreateInvoice } from '@/app/ui/invoices/buttons';
+import { lusitana } from '@/app/ui/fonts';
+import { InvoicesTableSkeleton } from '@/app/ui/skeletons';
+import { Suspense } from 'react';
 
-
-const InvoicesPage = () => {
+export default async function Page() {
+    const query = '';
+    const currentPage = 1;
     return (
         <div className="w-full">
             <div className="flex w-full items-center justify-between">
@@ -13,14 +18,12 @@ const InvoicesPage = () => {
                 <Search placeholder="Search invoices..." />
                 <CreateInvoice />
             </div>
-            {/*  <Suspense key={query + currentPage} fallback={<InvoicesTableSkeleton />}>
-        <Table query={query} currentPage={currentPage} />
-      </Suspense> */}
+            <Suspense key={query + currentPage} fallback={<InvoicesTableSkeleton />}>
+                <Table query={query} currentPage={currentPage} />
+            </Suspense>
             <div className="mt-5 flex w-full justify-center">
                 {/* <Pagination totalPages={totalPages} /> */}
             </div>
         </div>
-    )
+    );
 }
-
-export default InvoicesPage
