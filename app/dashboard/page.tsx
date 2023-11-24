883545fcb9b7d26de73a9c608f099ba1c5d8abff
Daniel Mateu <danielmateu86@gmail.com>
2023-11-24 09:22:29 +0100

    streaming html

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,10 +1,12 @@
+import { Suspense } from "react";
 import { fetchLatestInvoices, fetchRevenue } from "../lib/data";
 import LatestInvoices from "../ui/dashboard/latest-invoices";
 import RevenueChart from "../ui/dashboard/revenue-chart";
 import { lusitana } from "../ui/fonts";
+import { RevenueChartSkeleton } from "../ui/skeletons";
 
 export default async function DashboardPage() {
-    const revenue = await fetchRevenue()
+    // const revenue = await fetchRevenue()
     const latestInvoices = await fetchLatestInvoices()
 
     return (
@@ -23,7 +25,9 @@ export default async function DashboardPage() {
         /> */}
             </div>
             <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
-                <RevenueChart revenue={revenue} />
+                <Suspense fallback={<RevenueChartSkeleton />}>
+                    <RevenueChart />
+                </Suspense>
                 <LatestInvoices latestInvoices={latestInvoices} />
             </div>
         </main>
