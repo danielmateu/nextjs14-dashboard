a8b4378106785c8e6b072c96c54f0df865612606
Daniel Mateu <danielmateu86@gmail.com>
2023-11-24 13:03:19 +0100

    zod added

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -1,1 +1,12 @@
 'use server'
+
+export async function createInvoice(formData: FormData) {
+    // console.log('createInvoce', formData);
+    const rawFormData = {
+        customerId: formData.get('customerId'),
+        amount: formData.get('amount'),
+        status: formData.get('status'),
+    }
+
+    console.log('rawFormData', rawFormData);
+}
