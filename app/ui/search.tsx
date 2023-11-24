08749b947fb6be7d59e295757a1dc5de0180b1be
Daniel Mateu <danielmateu86@gmail.com>
2023-11-24 12:57:25 +0100

    server actions init

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -2,6 +2,9 @@
 
 import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
 import { usePathname, useRouter, useSearchParams } from 'next/navigation';
+import { useDebouncedCallback } from 'use-debounce';
+
+const WAIT_BETWEEN_SEARCH = 500;
 
 export default function Search({ placeholder }: { placeholder: string }) {
 
@@ -9,15 +12,19 @@ export default function Search({ placeholder }: { placeholder: string }) {
   const pathname = usePathname();
   const { replace } = useRouter();
 
-  const handleSearch = (term: string) => {
+
+  const handleSearch = useDebouncedCallback((term: string) => {
+    console.log(term);
     const params = new URLSearchParams(searchParams);
     if (term) {
       params.set('query', term)
     } else {
       params.delete('query')
     }
+    params.set('page', '1');
+
     replace(`${pathname}?${params.toString()}`);
-  }
+  }, WAIT_BETWEEN_SEARCH)
 
   return (
     <div className="relative flex flex-1 flex-shrink-0">
