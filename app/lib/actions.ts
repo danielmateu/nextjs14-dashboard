e2e5d5012230d9143bbee67dec86b527ed1b5187
Daniel Mateu <danielmateu86@gmail.com>
2023-11-24 13:32:14 +0100

    fixing issues

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -1,12 +1,48 @@
 'use server'
 
+import { z } from "zod";
+import { sql } from "@vercel/postgres";
+import { Invoice } from './definitions';
+import { revalidatePath } from 'next/cache';
+import { redirect } from "next/navigation";
+
+
+const CreateInvoiceSchema = z.object({
+    id: z.string(),
+    customerId: z.string(),
+    amount: z.coerce.number(),
+    status: z.enum(['pending', 'paid']),
+    date: z.string()
+});
+
+const CreateInvoiceFormSchema = CreateInvoiceSchema.omit({
+    id: true,
+    date: true
+});
+
+
 export async function createInvoice(formData: FormData) {
-    // console.log('createInvoce', formData);
-    const rawFormData = {
+    const { customerId, amount, status } = CreateInvoiceFormSchema.parse({
         customerId: formData.get('customerId'),
         amount: formData.get('amount'),
         status: formData.get('status'),
-    }
+    })
 
-    console.log('rawFormData', rawFormData);
+    console.log({
+        customerId,
+        amount,
+        status
+    });
+
+    const amountInCents = amount * 100;
+    // fecha actual
+    const date = new Date().toISOString().split('T')[0];
+
+    await sql`
+    INSERT INTO invoices (customer_id, amount, status, date)
+    VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
+    `;
+
+    revalidatePath('/dashboard/invoices');
+    redirect('/dashboard/invoices');
 }
